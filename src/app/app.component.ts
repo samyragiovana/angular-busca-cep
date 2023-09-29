488248bacb300960d06817fdbd5f2fce63af5980
SAMYRA GIOVANA CARVALHO CAMPOS <0064125@IFMG.EDU.BR>
2023-09-29 15:19:20 -0300

    busca cep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepService } from './service/cep.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'buscaCep';
+  cep:string = ''
+  cepData: any = []
+
+  constructor(private cepService:CepService){}
+  pesquisarCep(){
+    this.cepService.getCep(this.cep).subscribe((data:any) =>{
+      this.cepData = data
+      console.log(this.cepData)
+    }
+    )
+  }
 }
